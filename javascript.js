f0a7b8a79dcd06f7a07bbf7668dcc1aac386b4d0
EPedley <lizzypedley@gmail.com>
2023-03-11 15:48:05 +0000

    Added an error message if user tries to divide by 0

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let runningTotal = "";
 let num = "";
-let functionCounter = 0;
+let operander = "";
+let operanderCount = 0;
 
 const result = document.querySelector(".result");
 const calculation = document.querySelector(".calculation");
@@ -23,88 +24,152 @@ function divide(a, b) {
     return +a / +b;
 }
 
+function operate(a, b, operander, tempOperander) {
+
+        if (operander === "+") {
+            runningTotal = add(a, b);
+            if (tempOperander === "=") {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = "";
+            }
+            else {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = `${runningTotal} ${tempOperander}`;
+            }
+        num = "";
+        }
+
+        else if (operander === "-") {
+            runningTotal = subtract(a, b);
+            if (tempOperander === "=") {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = "";
+            }
+            else {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = `${runningTotal} ${tempOperander}`;
+            }
+        num = "";
+        }
+
+        else if (operander === "x") {
+            runningTotal = multiply(a, b);
+            if (tempOperander === "=") {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = "";
+            }
+            else {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = `${runningTotal} ${tempOperander}`;
+            }
+        num = "";
+        }
+
+        else if (operander === "÷") {
+            console.log(num);
+            if (num == 0) {
+                window.alert("You can't divide by zero!");
+                clearFunction();
+                return;
+            }
+
+            runningTotal = divide(a, b);
+            if (tempOperander === "=") {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = "";
+            }
+            else {
+                result.innerHTML = runningTotal;
+                calculation.innerHTML = `${runningTotal} ${tempOperander}`;
+            }
+        num = "";
+        }
+
+        else if (operander === "=") {
+            result.innerHTML = runningTotal;
+            calculation.innerHTML = `${runningTotal} ${tempOperander}`;
+        }
+}
+
 //concatenate numbers
 function createNumber(a) {
     return num += a;
 }
 
-//end of functions
-
 //when a user clicks a number, change a
 document.querySelectorAll('#number-buttons').forEach(button => {
     button.addEventListener('click', (e) => {
         let a = e.target.value;
-        num = createNumber(a);
-        result.innerHTML = num;        
+        if (num.length <= 17) {
+            num = createNumber(a);
+        }       
+        result.innerHTML = num;     
     })
 });
 
 //when a user clicks a function, perform that function
 document.querySelectorAll('#function-buttons').forEach(button => {
-    button.addEventListener('click', (e) => {
-        // if (e.target.value = "÷") {
-        //     //perform the function, i.e. + - x
-        //     let temp = 1;
-        //     runningTotal = divide(num, temp);
-        //     //show the user the result so far
-        //     result.innerHTML = runningTotal;
-        //     //show the user the calculation so far
-        //     calculation.innerHTML = `${runningTotal} + `;
-        //     //reset the number the user is now typing
-        //     num = "";
-        // }
-        
-        // // else if (e.target.value = "x") {
-        // //     //perform the function, i.e. + - x
-        // //     runningTotal = multiply(runningTotal, num);
-        // //     //show the user the result so far
-        // //     result.innerHTML = runningTotal;
-        // //     //show the user the calculation so far
-        // //     calculation.innerHTML = `${runningTotal} + `;
-        // //     //reset the number the user is now typing
-        // //     num = "";
-        // // }
+    button.addEventListener('click', function()  {
+        ++operanderCount;
+        let tempOperander = button.value
 
-        // if (e.target.value = "-") {
-        //     //perform the function, i.e. + - x
-        //     runningTotal = subtract(runningTotal, num);
-        //     //show the user the result so far
-        //     result.innerHTML = runningTotal;
-        //     //show the user the calculation so far
-        //     calculation.innerHTML = `${runningTotal} - `;
-        //     //reset the number the user is now typing
-        //     num = "";
-        //     functionCounter++;
-        // }
+        //the first time the user clicks a function button we dont need to perform a functio
+        if (operanderCount === 1) {
+            if (button.value === "=") {
+                if (num === "") {
+                    runningTotal = 0
+                }
+                else {
+                    runningTotal = num;
+                }
+                num = "" ;
+                calculation.innerHTML = runningTotal; 
+                operander = button.value;
+            }
 
-        if (e.target.value = "+") {
-            //perform the function, i.e. + - x
-            runningTotal = add(runningTotal, num);
-            //show the user the result so far
-            result.innerHTML = runningTotal;
-            //show the user the calculation so far
-            calculation.innerHTML = `${runningTotal} + `;
-            //reset the number the user is now typing
-            num = "";
+            else {
+                runningTotal = num;
+                num = "";
+                calculation.innerHTML = `${runningTotal} ${tempOperander}`;
+                operander = button.value;
+            }
+        }
+
+        else if (operanderCount > 1) {
+            operate(runningTotal, num, operander, tempOperander);
+            //change the operander after the function is called so that it is saved for the next user click
+            operander = button.value;
         }
     })
 });
 
 //clear function
+// const clear = document.getElementById("clear");
+// clear.addEventListener("click", function() {
+//     runningTotal = 0;
+//     num = "";
+//     calculation.innerHTML = "";
+//     result.innerHTML = runningTotal;
+//     operanderCount = 0;
+// });
+
+//clear function
 const clear = document.getElementById("clear");
-clear.addEventListener("click", function () {
+clear.addEventListener("click", clearFunction);
+
+function clearFunction() {
     runningTotal = 0;
     num = "";
     calculation.innerHTML = "";
     result.innerHTML = runningTotal;
+    operanderCount = 0;
+}
+
+//delete function
+const deleteClicked = document.getElementById("delete");
+deleteClicked.addEventListener("click", function(){
+    let numArray = Array.from(num);
+    numArray.splice(-1)
+    num = numArray.join("");
+    result.innerHTML = num;
 });
-
-// const colourModeClicked = document.querySelector(".colour-mode");
-// colourModeClicked.addEventListener("click", function() {
-//     paintSetting = "colour";
-// });
-
-// console.log(add(a, b));
-// console.log(subtract(a, b));
-// console.log(multiply(a, b));
-// console.log(divide(a, b));
